d259a312fb0f2d6984a45e062ad7886d8609a7b0
Brendenbean <BWells5@cps.edu>
2026-02-06 14:48:36 +0000

    wrrg

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let venue = document.querySelector('.room');
 let paraContent = document.querySelector('.yeet');
 let classroom = document.querySelector('.classroom');
 let currentIndex = 0;
-blues.style.display = "none";
+
 
 
 
@@ -27,12 +27,12 @@ setInterval(nextImage, 3000);
 venue.addEventListener('click', function(){
     blues.style.display = "block";
     content.style.display = "none";
-paraContent.innerHTML = "we also hold many different event for many different occasions but we specialize in the rich blues feels";
+paraContent.innerHTML = "this would be the layout of the venue";
 });
 
 classroom.addEventListener('click', function(){
     blues.style.display = "none";
     content.style.display = "block";
-paraContent.innerHTML = "Your children, family friend, or yourself even can learn beautiful insturment that blues music was played on like guitars and rums for a small fee.";
+paraContent.innerHTML = "this is the place the venue will be at Regal theater";
 });
 
